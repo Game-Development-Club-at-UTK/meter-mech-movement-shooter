26058c172b88a1d636d42adb27fa37fa9112f3b6
Camden Baxter <camdenbaxter+git@protonmail.com>
2025-09-29 19:30:20 -0400

    Revert "Merge branch 'main' of https://github.com/Game-Development-Club-at-UTK/meter-mech-movement-shooter"
    This reverts commit 6de14d670dbeac25d6215dea82dd3de5c475205f, reversing
    changes made to 5b05dc807d3b6866c5c9faa2ced242b6e33ba936.

--- src/player.gd
+++ src/player.gd
@@ -3,36 +3,9 @@ class_name Player
 
 
 @export var run_speed = 300.0
-@export var dash_speed = 1200.0
 @export var jump_force = 10.0
-@export var gravity = 30.0
-@export var mouse_sensitivity = 0.005
-@export var max_health = 100.0
-@export var fire_rate = 0.1
-@export var damage = 2.0
-@export var heat_gain = 0.01
-@export var dash_length = 5.0
-@export var dash_heat_cost = 0.2
-
-static var GROUNDED_STATE = GroundedState.new()
-static var IN_AIR_STATE = InAirState.new()
-static var DASHING_STATE = DashingState.new()
-
-var health = max_health
-var heat = 0.0
-var current_state: State = IN_AIR_STATE
-
-
-func _ready():
-	GROUNDED_STATE.name = "GroundedState"
-	add_child(GROUNDED_STATE)
-	IN_AIR_STATE.name = "InAirState"
-	add_child(IN_AIR_STATE)
-	DASHING_STATE.name = "DashingState"
-	add_child(DASHING_STATE)
-	DASHING_STATE.create_timer(self)
-
-	current_state.on_enter(self)
+@export var gravity = 10
+@export var mouse_sensitivity = 0.002
 
 var maxHeat = 100.0
 var heatLoss = 5.0
@@ -45,38 +18,15 @@ var frameTimer = 0.0
 
 # It might be a good idea to replace most of this movement code with a state machine later on
 func _physics_process(delta):
-
-	if Input.is_action_pressed("fire") and $FireCooldown.is_stopped():
-		var collider = $RayCast3D.get_collider()
-		if collider is Enemy:
-			collider.health -= damage
-		heat += heat_gain
-		$FireCooldown.start(fire_rate)
-
-
 	#tiemr management
 	frameTimer += delta
 	if frameTimer >= 1:
 		frameTimer = 0
 	
-
 	var view_velocity = -Input.get_last_mouse_velocity() * mouse_sensitivity * delta
 	rotation.y += view_velocity.x
 	$Camera3D.rotation.x += view_velocity.y
 	$Camera3D.rotation.x = clampf ($Camera3D.rotation.x, -PI/2, PI/2)
-
-
-	# State machine
-	# Do movement logic depending on current state, and transition to new states under certain conditions
-	var new_state = current_state.proc(self, delta)
-	if new_state is State:
-		current_state.on_exit(self)
-		current_state = new_state
-		current_state.on_enter(self)
-
-
-
-	heat = clampf(heat, 0.0, 1.0)
 
 	var h_velocity = Vector3()
 
@@ -132,4 +82,3 @@ func _physics_process(delta):
 		currentHeat -= heatLoss * delta
 	$"../UI/ProgressBar".value = lerpf($"../UI/ProgressBar".value, currentHeat, .2)
 	$"../UI/ProgressBar2".value = lerpf($"../UI/ProgressBar2".value, currentHeat, .2)
-
--- src/player_states/in_air.gd
+++
@@ -1,9 +0,0 @@
-extends State
-class_name InAirState
-
-
-func proc(player: Player, delta):
-	if player.is_on_floor_only():
-		return player.GROUNDED_STATE
-
-	return super(player, delta)
